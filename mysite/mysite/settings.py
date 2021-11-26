c4152af51eb839cc894adb3617ca1d506b362c91
Wookjae Maeng <wjmaeng@gmail.com>
2021-11-26 17:07:37 +0900

    secret key 삭제, allowed host 추가

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -19,13 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%ryi_3oaq-5k8mr2ios$&8w^=i^o#%gf*zk#cqo&hukw+7(0mh'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.34.130.76']
 
 
 # Application definition
