5db70f02febde73474da17275b48366f9d899576
Wookjae Maeng <wookjae@eigene.ai>
2021-11-27 14:40:15 +0900

    allowed_hosts

--- mysite/mysite/settings/prod.py
+++ mysite/mysite/settings/prod.py
@@ -2,4 +2,4 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.34.130.76', 'ec2-3-34-130-76.ap-northeast-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ["ec2-3-34-130-76.ap-northeast-2.compute.amazonaws.com"]
